7b84e859207454f00fde410d55a9abde527d8da5
teddy-beau <hello@teddy-beau.com>
2021-02-08 11:37:32 +0100

    updated code

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,34 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Tasks = ({ tasks, setTasks }) => {
+   // Function to define what happens when clicking "Add task" button
+   const handleCheck = (element, index) => {
+      // Upon clicking on the checkbox, if the "done" key is set to true, it is toggled to false
+      if (element.done) {
+         const newTasks = [...tasks];
+         const doneTask = tasks[index];
+         newTasks[index].done = false;
+         newTasks.splice(index, 1);
+         newTasks.unshift(doneTask);
+         setTasks(newTasks);
+         // Else, it is toggled to true
+      } else {
+         const newTasks = [...tasks];
+         const doneTask = tasks[index];
+         newTasks[index].done = true;
+         newTasks.splice(index, 1);
+         newTasks.push(doneTask);
+         setTasks(newTasks);
+      }
+   };
+   // Function to handle task deletion
+   const handleDelete = (index) => {
+      const newTasks = [...tasks];
+      newTasks.splice(index, 1);
+      setTasks(newTasks);
+   };
+
    // Looping through the array of tasks:
    return tasks.map((element, index) => {
-      // Function to define what happens when clicking "Add task" button
-      const handleClick = () => {
-         // Upon clicking on the checkbox, if the "done" key is set to true, it is toggled to false
-         if (element.done) {
-            const newTasks = [...tasks];
-            const doneTask = tasks[index];
-            newTasks[index].done = false;
-            newTasks.splice(index, 1);
-            newTasks.unshift(doneTask);
-            setTasks(newTasks);
-            // Else, it is toggled to true
-         } else {
-            const newTasks = [...tasks];
-            const doneTask = tasks[index];
-            newTasks[index].done = true;
-            newTasks.splice(index, 1);
-            newTasks.push(doneTask);
-            setTasks(newTasks);
-         }
-      };
       return (
          <div className="task" key={index}>
             <input
                type="checkbox"
                id={index}
-               onClick={handleClick}
+               onClick={() => handleCheck(element, index)}
                // If the task is done the checkbox is checked
                checked={tasks[index].done === false ? false : true}
             />
@@ -49,11 +56,7 @@ const Tasks = ({ tasks, setTasks }) => {
                icon="backspace"
                color="#4eb5a4"
                // Upon clicking of the icon, the current task object will be removed from the array of tasks
-               onClick={() => {
-                  const newTasks = [...tasks];
-                  newTasks.splice(index, 1);
-                  setTasks(newTasks);
-               }}
+               onClick={() => handleDelete(index)}
             />
          </div>
       );
